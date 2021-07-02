4c0f8806212924da36661285c29fed26f640e32a
Polidora90 <alessandra.torry@gmail.com>
2021-07-02 16:12:27 +0200

    creates form

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -3,7 +3,27 @@
 @section('content')
 
 <div class="container">
-    <h2>Modifica un post - area privata</h2>
+    <h2> {{ __('Ciao ' . Auth::user()->name . ', crea il tuo nuovo post:') }}</h2>
+
+        <form action="{{ route('admin.posts.update') }}" method='post'>
+    @csrf
+
+    @method('PATCH')
+
+        <div class="mb-3">
+            <label for="title" class="form-label">Titolo</label>
+            <input type="text" name="title" id="title" class="form-control" value="{{ $post->title }}"> <br>
+        </div>
+      
+        <div class="mb-3">
+            <label for="content" class="form-label">Contenuto</label>
+            <textarea type="text" name="content" id="content" rows="10" cols="50" class="form-control"> {{ $post->content }} </textarea><br>
+        </div>
+
+
+        <input type="submit" value="Salva modifiche" class="btn btn-outline-secondary">
+    
+    </form>
 
 </div>
 
