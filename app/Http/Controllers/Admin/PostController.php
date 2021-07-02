7d5799afd6a2edba1413368f8f802795339620a8
Polidora90 <alessandra.torry@gmail.com>
2021-07-02 16:20:40 +0200

    function show

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -49,9 +49,18 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        //
+        //mostra il primo post where la col 'slug' ha il valore di $slug
+        $post = Post::where('slug', $slug)->first();
+
+        if(!$post) {
+            abort(404);
+        }
+
+        $data = ['post' => $post];
+
+        return view('posts.show', $data);
     }
 
     /**
