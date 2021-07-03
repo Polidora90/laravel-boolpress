373caab8d62634802ea152dac111d93ec6a4d5af
Polidora90 <alessandra.torry@gmail.com>
2021-07-03 14:04:53 +0200

    adds show function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,19 @@ class PostController extends Controller
         ];
         return view('posts.index', $data);
     }
+
+
+    public function show($slug)
+    {
+        //mostra il primo post where la col 'slug' ha il valore di $slug
+        $post = Post::where('slug', $slug)->first();
+
+        if(!$post) {
+            abort(404);
+        }
+
+        $data = ['post' => $post];
+
+        return view('posts.show', $data);
+    }
 }
