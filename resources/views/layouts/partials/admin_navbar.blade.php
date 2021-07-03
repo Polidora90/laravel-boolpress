74e9ad16e14fc4ac2d45ae58586f683aa287a97e
Polidora90 <alessandra.torry@gmail.com>
2021-07-03 21:46:57 +0200

    stilizza admin navbar

--- resources/views/layouts/partials/admin_navbar.blade.php
+++ resources/views/layouts/partials/admin_navbar.blade.php
@@ -2,7 +2,7 @@
     <div>
         <ul class="navbar-nav my-navbar-nav">
             <li><a href="#">Dashboard</a></li>
-            <li><a href="#">Posts</a></li>
+            <li><a href="{{ route('admin.posts.index') }}">Posts</a></li>
             <li><a href="#">Users</a></li>
             <li><a href="#">Categories</a></li>
             <li><a href="#">Tags</a></li>
