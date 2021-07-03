fa78c93d9e5e42c5cad60610a6b12fbbe0a75dc7
Polidora90 <alessandra.torry@gmail.com>
2021-07-03 13:25:31 +0200

    view public posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,6 +5,21 @@
 <div class="container">
     <h2>Post pubblici</h2>
     {{-- Qui dovrò stampare le anteprime dei post e aggiungere il tasto "dettagli" per vederlo interamente --}}
+    
+    @foreach($posts as $post)
+    <div class="card mb-3 pt-3">
+        <div class="my-img-container">
+            <img src="{{ asset('images/placeholder.png') }}" class="card-img-top" alt="placeholder">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">{{ $post->title }}</h5>
+            <p class="card-text">{{Str::limit($post->content, 200)}}</p>
+            <p class="card-text"><small class="text-muted">Creazione: {{ $post->created_at }}</small></p>
+            <p class="card-text"><small class="text-muted">Ultima modifica: {{ $post->updated_at }}</small></p>
+        </div>
+    </div>
+    @endforeach
+  
 
 </div>
 
