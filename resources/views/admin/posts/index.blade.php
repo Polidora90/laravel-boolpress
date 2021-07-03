b5d3cbcb7f1c45609a8136582a137960f3824ab0
Polidora90 <alessandra.torry@gmail.com>
2021-07-03 21:47:31 +0200

    aggiunge tabella post area privata

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,11 +3,42 @@
 @section('content')
 
 <div class="container">
-    <h2>Post area privata</h2>
+    <div class="d-flex justify-content-between">
+        <h2>Tutti i post - area privata</h2>
+        <a href="{{ route('admin.posts.create') }}" class="btn btn-outline-primary mb-2">Crea un nuovo post</a>
+    </div>
     {{-- Qui saranno mostrati i post scritti dall'utente e potranno essere anche modificati o eliminati --}}
-    <a href="#">Dettagli del post</a>
-    <a href="#">Modifica il post</a>
-    <a href="#">Elimina il post</a>
+    {{-- bisognerebbe fare un filtro per mostrare solo quelli scritti dall'utente --}}
+    {{-- fare un link che lo porta all'index pubblico per vedere tutti i post --}}
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titolo</th>
+                <th>Slug</th>
+                <th>Categoria</th>
+                <th class="text-center">Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($posts as $post)
+            <tr>
+                <td>{{ $post->id }}</td>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->slug }}</td>
+                <td>-</td>
+                <td>
+                    <a href="#">Dettagli</a>
+                    <a href="#">Modifica</a>
+                    <a href="#">Elimina</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    
+    </table>
+
 
 </div>
 
