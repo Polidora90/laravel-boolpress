384a58514a1d58ecb396029910ea412baeb2dfc3
Polidora90 <alessandra.torry@gmail.com>
2021-07-04 12:47:34 +0200

    cambia il nome del blog

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Polodora blog'),
+    'name' => env('APP_NAME', 'Polodora Blog'),
 
     /*
     |--------------------------------------------------------------------------
