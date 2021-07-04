fbba3d95f0113d162532ce35423cd7fcca7c9812
Polidora90 <alessandra.torry@gmail.com>
2021-07-04 14:49:29 +0200

    aggiunge collegamento tra tab posts e users

--- database/migrations/2021_07_04_111949_add_foreign_key_to_posts_table.php
+++ database/migrations/2021_07_04_111949_add_foreign_key_to_posts_table.php
@@ -36,7 +36,7 @@ class AddForeignKeyToPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->dropForeign('user_id');
+            $table->dropForeign('posts_user_id_foreign');
             $table->dropColumn('user_id');
         });
     }
