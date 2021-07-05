77e8025a66335b909f41a8b50b5b7cbc8fcb219a
Polidora90 <alessandra.torry@gmail.com>
2021-07-05 16:20:05 +0200

    stampa categoria

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,6 +16,7 @@
             <p class="card-text">{{Str::limit($post->content, 200)}}</p>
             <a href="{{ route('posts.show', ['slug' => $post->slug]) }}" class="btn btn-outline-secondary btn-sm mb-2">Continua a leggere...</a>
             <p class="card-text my-created-at"><small class="text-muted">Creazione: {{ $post->created_at }}</small></p>
+            <p class="card-text my-created-at"><small class="text-muted">Categoria: {{ $post->category ? $post->category->name : '-' }} </small></p>
             <p class="card-text"><small class="text-muted">Ultima modifica: {{ $post->updated_at }}</small></p>
         </div>
     </div>
