4c831881b4d62ce1b961d3d2750cb710435fbb45
Polidora90 <alessandra.torry@gmail.com>
2021-07-05 15:20:30 +0200

    aggiunge categoria

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -11,6 +11,7 @@
         <div class="card-body">
             <h5 class="card-title">{{ $post->title }}</h5>
             <p class="card-text">{{ $post->content }}</p>
+            <p class="card-text my-created-at"><small class="text-muted">Categoria: {{ $post->category->name }}</small></p>
             <p class="card-text my-created-at"><small class="text-muted">Creazione: {{ $post->created_at }}</small></p>
             <p class="card-text"><small class="text-muted">Ultima modifica: {{ $post->updated_at }}</small></p>
             <p class="card-text"><small class="text-muted">Utente: {{ $post->user->name }} ({{ $post->user->email }})</small></p>
