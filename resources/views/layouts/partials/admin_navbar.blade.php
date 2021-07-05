e677dc9471bd336f11dd046b4afb604aa3484cf8
Polidora90 <alessandra.torry@gmail.com>
2021-07-05 22:02:19 +0200

    applica classe active

--- resources/views/layouts/partials/admin_navbar.blade.php
+++ resources/views/layouts/partials/admin_navbar.blade.php
@@ -1,11 +1,11 @@
 @section('admin_navbar')
     <div>
         <ul class="navbar-nav my-navbar-nav">
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="{{ route('admin.posts.index') }}">Posts</a></li>
-            <li><a href="#">Users</a></li>
-            <li><a href="{{ route('admin.categories.index') }}">Categories</a></li>
-            <li><a href="{{ route('admin.tags.index') }}">Tags</a></li>
+            <li><a href="#" class="{{ Request::is('admin') ? 'active' : '' }}">Dashboard</a></li>
+            <li class="{{ Request::is('admin/posts*') ? 'active' : '' }}"><a href="{{ route('admin.posts.index') }}">Posts</a></li>
+            <li class="{{ Request::is('admin/users*') ? 'active' : '' }}"><a href="#">Users</a></li>
+            <li class="{{ Request::is('admin/categories*') ? 'active' : '' }}"><a href="{{ route('admin.categories.index') }}">Categories</a></li>
+            <li class="{{ Request::is('admin/tags*') ? 'active' : '' }}"><a href="{{ route('admin.tags.index') }}">Tags</a></li>
         </ul>
     </div>
 @endsection
