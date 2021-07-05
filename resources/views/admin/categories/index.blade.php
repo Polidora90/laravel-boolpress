fa2d2d2e6efc11c05e573a3f27eaaeb75e40017e
Polidora90 <alessandra.torry@gmail.com>
2021-07-05 16:07:52 +0200

    conta i post per ogni categoria

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -5,12 +5,14 @@
 @section('content')
 
 <div class="container">
+    <h2>Tutte le categorie</h2>
     <table class="table">
         <thead>
             <tr>
                 <th>ID</th>
                 <th>Nome</th>
                 <th>Slug</th>
+                <th>Numero Post</th>
                 <th>Data Creazione</th>
                 {{-- in caso di crud --}}
                 {{-- <th class="text-center">Azioni</th> --}}
@@ -22,6 +24,8 @@
                 <td>{{ $category->id }}</td>
                 <td>{{ $category->name }}</td>
                 <td>{{ $category->slug }}</td>
+                {{-- posso usare il count perchè la proprietà tra parentesi ci torna tutti i post esistenti di quella categoria sottoforma di array --}}
+                <td>{{ count($category->posts) }}</td>
                 <td>{{ $category->created_at}}</td>
                 {{-- <td>
                     <a href="{{ route('admin.posts.show', ['post' => $post ->id]) }}">Dettagli</a>
