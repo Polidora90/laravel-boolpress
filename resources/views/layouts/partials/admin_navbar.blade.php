cb16682b8a1c50d48cc168d0549cc0be217e78a8
Polidora90 <alessandra.torry@gmail.com>
2021-07-07 19:31:35 +0200

    debugga assegnazione class active

--- resources/views/layouts/partials/admin_navbar.blade.php
+++ resources/views/layouts/partials/admin_navbar.blade.php
@@ -1,11 +1,11 @@
 @section('admin_navbar')
     <div>
         <ul class="navbar-nav my-navbar-nav">
-            <li><a href="#" class="{{ Request::is('admin') ? 'active' : '' }}">Dashboard</a></li>
-            <li class="{{ Request::is('admin/posts*') ? 'active' : '' }}"><a href="{{ route('admin.posts.index') }}">Posts</a></li>
-            <li class="{{ Request::is('admin/users*') ? 'active' : '' }}"><a href="#">Users</a></li>
-            <li class="{{ Request::is('admin/categories*') ? 'active' : '' }}"><a href="{{ route('admin.categories.index') }}">Categories</a></li>
-            <li class="{{ Request::is('admin/tags*') ? 'active' : '' }}"><a href="{{ route('admin.tags.index') }}">Tags</a></li>
+            <li><a href="{{ route('admin.index') }}" class="{{ Request::is('admin') ? 'active' : '' }}">Dashboard</a></li>
+            <li><a href="{{ route('admin.posts.index') }}" class="{{ Request::is('admin/posts*') ? 'active' : '' }}">Posts</a></li>
+            <li><a href="#" class="{{ Request::is('admin/users*') ? 'active' : '' }}">Users</a></li>
+            <li><a href="{{ route('admin.categories.index') }}" class="{{ Request::is('admin/categories*') ? 'active' : '' }}">Categories</a></li>
+            <li><a href="{{ route('admin.tags.index') }}" class="{{ Request::is('admin/tags*') ? 'active' : '' }}">Tags</a></li>
         </ul>
     </div>
 @endsection
