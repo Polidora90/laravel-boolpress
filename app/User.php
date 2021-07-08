7236fcc1e0faf24398d5e0f0f078c61ad8e8eb4f
Polidora90 <alessandra.torry@gmail.com>
2021-07-08 09:29:24 +0200

    aggiunge relazione tra dettagli e utenti

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Post');
     }
+
+    public function userDetail()
+    {
+        return $this->hasOne('App\UserDetail');
+    }
 }
