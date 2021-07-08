616e4d7f2db9e2b0afe61eefee2fea7b223dd8c2
Polidora90 <alessandra.torry@gmail.com>
2021-07-08 09:22:21 +0200

    crea tabella users details

---
+++ database/migrations/2021_07_08_071307_create_users_details_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('address');
+            $table->string('city');
+            $table->string('province');
+            $table->string('zip');
+            $table->date('birthDate');
+            $table->string('birthCountry');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_details');
+    }
+}
